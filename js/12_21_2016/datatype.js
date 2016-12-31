67fa10b347204fa8aa4b217a2ef60ccb22fc6363
Nani Iraju <Nani Iraju>
2016-12-30 19:50:02 -0600

    JS-III Class,Objects

---
+++ js/12_21_2016/datatype.js
@@ -0,0 +1,35 @@
+//Data types Numbers initialization
+var x = 10;
+var y = 23.3;
+
+// String type variable initialization
+
+var firstName = 'NARESH';
+var lastname = 'itharaju';
+
+
+// boolean type variables initialization
+
+var present = true;
+var absent = false;
+
+
+//null type variable initialization
+
+var n =  null;
+
+// undefined data type initialization
+
+var a = undefined;
+
+console.log(x);
+console.log(y);
+console.log(firstName);
+console.log(present);
+console.log(n);
+console.log(a);
+
+
+
+
+
---
+++ js/objects/objects.js
@@ -0,0 +1,24 @@
+var x ={
+    firstName: 'naresh',
+    section: 'A',
+    rollnumber: 88,
+    ispresent: true,
+    pre: null,
+     z: {
+            ispresent: false,
+            firstName: 'bindhu',
+
+        }
+
+
+    y: {
+        firstName:'sunitha',
+        lastNamae: 'velimineti',
+        rollnumber: 55
+     }
+
+ }
+
+ console.log(x);
+ console.log(x.y);
+
